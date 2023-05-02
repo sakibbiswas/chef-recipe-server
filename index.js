151a2c6c699b1d7df0a0aa9af2dfbe8dce854f18
sakibbiswas <sakibsakib99880@gmail.com>
2023-05-02 23:47:21 +0600

    chefdata add

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors')
 const countries = require('.//Data/Countries.json')
+const chefdata = require('.//Data/chefdata.json')
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -11,6 +12,26 @@ app.get('/', (req, res) => {
 
 app.get('/countries', (req, res) => {
   res.send(countries)
+})
+app.get('/countries/:id', (req, res) => {
+    const id =parseInt (req.params.id);
+    if (id === 0) {
+       res.send(chefdata) 
+    }
+    else {
+          const countrydata=chefdata.filter(c=>parseInt(c.category_id)===id)
+  res.send(countrydata)  
+    }
+
+})
+app.get('/chef', (req, res) => {
+   res.send(chefdata)
+})
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    const selectechefdata = chefdata.find(c => c._id === id)
+    res.send(selectechefdata)
 })
 app.listen(port, () => {
   console.log(` Recipe API is running  on port : ${port}`)
